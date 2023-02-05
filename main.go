83b08d3782fcf7136e380d7bdf7c7dfdc503a71f
ruvarma <ruvarma@Ubuntu.myguest.virtualbox.org>
2023-02-05 23:04:07 +0530

    hostnamespace

--- main.go
+++ main.go
@@ -14,19 +14,32 @@ func main() {
 	switch os.Args[1] {
 	case "run":
 		run()
+	case "child":
+		child()
 	default:
 		panic("Bad Command")
 	}
 }
 
 func run() {
-	fmt.Printf("Running %v\n", os.Args[2:])
-	cmd := exec.Command(os.Args[2], os.Args[3:]...)
+	fmt.Printf("Running %v as %d\n", os.Args[2:], os.Getpid())
+	cmd := exec.Command("/proc/self/exe", append([]string{"child"}, os.Args[2:]...)...)
 	cmd.Stdin = os.Stdin
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 	cmd.SysProcAttr = &syscall.SysProcAttr{
-		Cloneflags: syscall.CLONE_NEWUTS,
+		Cloneflags: syscall.CLONE_NEWUTS | syscall.CLONE_NEWPID,
 	}
+	cmd.Run()
+}
+
+func child() {
+	fmt.Printf("Running %v as %d\n", os.Args[2:], os.Getpid())
+	cmd := exec.Command(os.Args[2], os.Args[3:]...)
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	syscall.Sethostname([]byte("container"))
+
 	cmd.Run()
 }
